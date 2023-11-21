85d0c4afb12863cb27e4c693b57f1ad8eb295d6c
Kaz <zakfloreta20@gmail.com>
2023-11-22 00:41:36 +0800

    haha

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ function calculate() {
     var expression = document.getElementById("display").innerHTML;
     var result = eval(expression);
     document.getElementById("display").innerHTML = result;
-}
+} 
